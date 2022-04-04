fc6910c237df3ec932bbce87b34ed4c958755c37
vakhairulislam <tubebusy@gmail.com>
2022-04-04 22:02:08 +0600

    404 Created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AllReviews from './components/AllReviews/AllReviews';
 import Dashboard from './components/Dashboard/Dashboard';
 import Blogs from './components/Blogs/Blogs';
 import About from './components/About/About'
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
 
@@ -23,7 +24,7 @@ function App() {
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
-        <Route path='*' element={<Home></Home>}> </Route>
+        <Route path='*' element={<NotFound></NotFound>}> </Route>
       </Routes>
     </div>
   );
