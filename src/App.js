ce05f46bc3fe689803169704c9b7c1dc9c862715
vakhairulislam <tubebusy@gmail.com>
2022-04-04 23:10:01 +0600

    Final Edit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,9 @@ import About from './components/About/About'
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
-
   return (
-
     <div className="App">
-
       <Header></Header>
-
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
